87edfa28678c69fbb5c2ad5ebf70ee46eb520fd1
JS.Maeng <maeng@telechips.com>
2019-12-10 17:58:25 +0900

    hello thread : add mutex
    Change-Id: Ib3d1ccee1995d8cfa0122c15d8952d73944de842

--- hello-thread/src/hello.c
+++ hello-thread/src/hello.c
@@ -2,23 +2,46 @@
 #include <unistd.h>
 #include <pthread.h>
 
+#include <stdlib.h> //rand
+
+typedef struct tHandle tHandle;
+
+struct tHandle
+{
+	int value;
+	pthread_mutex_t mutex;
+};
+
 void* threadfunc1(void *data)
 {
-	int indata = *(int*)data;
-	for(int i=0; i<10; i++)
+	tHandle* phandle = (tHandle*)data;
+	for(int i=0; i<1000; i++)
 	{
-		printf("hello thread 1, input data : %d\n",indata);
+		pthread_mutex_lock(&phandle->mutex);
+
+		phandle->value = 1;
+		rand(); //do something.
+		if(phandle->value != 1)
+			printf("race condition in thread 1!!!!\n"); //if there is no mutex it is occured.
+
+		pthread_mutex_unlock(&phandle->mutex);
 		usleep(1000);
 	}
 }
 
 void* threadfunc2(void *data)
 {
-	int indata = *(int*)data;
-	for(int i=0; i<15; i++)
+	tHandle* phandle = (tHandle*)data;
+	for(int i=0; i<1000; i++)
 	{
-		printf("hello thread 2, input data : %d\n",indata);
-		usleep(700);
+		pthread_mutex_lock(&phandle->mutex);
+		phandle->value = 2;
+		rand(); //do something.
+		if(phandle->value != 2)
+			printf("race condition in thread 2!!!!\n"); //if there is no mutex it is occured.
+
+		pthread_mutex_unlock(&phandle->mutex);
+		usleep(1000);
 	}
 }
 
@@ -26,17 +49,16 @@ int main()
 {
 	pthread_t p_thread[2];
 	int status;
-	int data[2] = {1,2};
-
-	pthread_create(&p_thread[0], NULL, threadfunc1, (void *)&data[0]);
-	pthread_create(&p_thread[1], NULL, threadfunc2, (void *)&data[1]);
+	tHandle handle;
+	handle.value = 0;
+	pthread_mutex_init(&handle.mutex, NULL);
+	pthread_create(&p_thread[0], NULL, threadfunc1, (void *)&handle);
+	pthread_create(&p_thread[1], NULL, threadfunc2, (void *)&handle);
 
 	pthread_join(p_thread[0], (void **)&status);
 	printf("thread exit 1\n");
-	
 	pthread_join(p_thread[1], (void **)&status);
 	printf("thread exit 2\n");
-		
 
 	return 0;
 }
